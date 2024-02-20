385b1568c73b424c5bdac9c84d181a8ea6da358d
Pranav <kheldarpranav03@gmail.com>
2024-02-20 22:26:03 +0530

    20/02 add FocusListner to Signup Class

--- electricity/billing/system/Signup.java
+++ electricity/billing/system/Signup.java
@@ -1,38 +1,43 @@
 package electricity.billing.system;
 
 import javax.swing.*;
+
+import com.mysql.cj.protocol.Resultset;
+
 import java.awt.Choice;
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.sql.ResultSet;
 
 // To apply action listener we need to impelent actionlistner to our class
 
-public class Signup extends JFrame implements ActionListener
-{
-    JTextField textmeterNo,textemployid, textusername, textname,textpassword;
+public class Signup extends JFrame implements ActionListener {
+    JTextField textmeterNo, textemployid, textusername, textname, textpassword;
     Choice Accountas;
-    JButton Create , Back;
-    Signup()
-    {
+    JButton Create, Back;
+
+    Signup() {
         super("Signup Page");
         // To change color of backgound of whole pannel
-        getContentPane().setBackground(new Color(168,203,255));
+        getContentPane().setBackground(new Color(168, 203, 255));
 
         JLabel title = new JLabel("Create - Account");
         add(title);
-        title.setBounds(0,0,120,20);
+        title.setBounds(0, 0, 120, 20);
 
         // Create Account As
         JLabel createAccountas = new JLabel("Create Account As:");
         add(createAccountas);
-        createAccountas.setBounds(30,50,120,20);
+        createAccountas.setBounds(30, 50, 120, 20);
         Accountas = new Choice();
         add(Accountas);
-        Accountas.setBounds(160,50,120,20);
+        Accountas.setBounds(160, 50, 120, 20);
         Accountas.add("Admin");
         Accountas.add("Coustomer");
 
@@ -44,7 +49,7 @@ public class Signup extends JFrame implements ActionListener
         textmeterNo = new JTextField();
         add(textmeterNo);
         textmeterNo.setVisible(false);
-        textmeterNo.setBounds(160, 100, 125,20);
+        textmeterNo.setBounds(160, 100, 125, 20);
 
         // Employer ID
         JLabel employeid = new JLabel("Employer id:");
@@ -54,7 +59,7 @@ public class Signup extends JFrame implements ActionListener
         textemployid = new JTextField();
         add(textemployid);
         textemployid.setVisible(true);
-        textemployid.setBounds(160, 100, 125,20);
+        textemployid.setBounds(160, 100, 125, 20);
 
         // Username
         JLabel username = new JLabel("Username:");
@@ -64,13 +69,37 @@ public class Signup extends JFrame implements ActionListener
         add(textusername);
         textusername.setBounds(160, 140, 125, 20);
 
-        // Name 
+        // Name
         JLabel name = new JLabel("Name");
         add(name);
         name.setBounds(30, 180, 125, 20);
-        textname = new JTextField();
+        textname = new JTextField("");
         add(textname);
         textname.setBounds(160, 180, 125, 20);
+
+        // To get name according to meter number
+        textmeterNo.addFocusListener(new FocusListener() {
+
+            @Override
+            public void focusGained(FocusEvent e) {
+
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                try {
+                    Database c = new Database();
+                    ResultSet resultSet = c.statement
+                            .executeQuery("SELECT * from Signup where meter_no = '" + textmeterNo.getText() + "'");
+                    if (resultSet.next()) {
+                        textname.setText(resultSet.getString("name"));
+                    }
+                } catch (Exception E) {
+
+                }
+            }
+
+        });
 
         // Password
         JLabel passWord = new JLabel("Password");
@@ -86,72 +115,68 @@ public class Signup extends JFrame implements ActionListener
             @Override
             public void itemStateChanged(ItemEvent e) {
                 String userchoice = Accountas.getSelectedItem();
-                if(userchoice.equals("Coustomer"))
-                {
-                    employeid.setVisible(false); 
+                if (userchoice.equals("Coustomer")) {
+                    employeid.setVisible(false);
                     textemployid.setVisible(false);
                     meterNo.setVisible(true);
                     textmeterNo.setVisible(true);
 
-                }
-                else
-                {
-                    employeid.setVisible(true); 
+                } else {
+                    employeid.setVisible(true);
                     textemployid.setVisible(true);
                     meterNo.setVisible(false);
                     textmeterNo.setVisible(false);
                 }
             }
-            
+
         });
 
         // Create Button
         Create = new JButton("Create");
-        Create.setBounds(40, 285, 100,25 );
+        Create.setBounds(40, 285, 100, 25);
         add(Create);
-        Create.setBackground(new Color(66,127,219)); //TO change backgound color of button
-        Create.setForeground(Color.WHITE);//TO change color of text of button
+        Create.setBackground(new Color(66, 127, 219)); // TO change backgound color of button
+        Create.setForeground(Color.WHITE);// TO change color of text of button
         Create.addActionListener(this); // to know wether button is click or not
 
         // Back Button
         Back = new JButton("Back");
         Back.setBounds(180, 285, 100, 25);
         add(Back);
-        Back.setBackground(new Color(66,127,219)); //TO change backgound color of button
-        Back.setForeground(Color.WHITE);//TO change color of text of button
-        Back.addActionListener(this);// to know wether button is click or not 
+        Back.setBackground(new Color(66, 127, 219)); // TO change backgound color of button
+        Back.setForeground(Color.WHITE);// TO change color of text of button
+        Back.addActionListener(this);// to know wether button is click or not
 
         // Image used for signup
         ImageIcon signupImg = new ImageIcon(ClassLoader.getSystemResource("icons\\sign-up.png"));
-       Image singupImg_2 = signupImg.getImage().getScaledInstance(250 , 250 , Image.SCALE_DEFAULT);
-       ImageIcon fprofileOne = new ImageIcon(singupImg_2);
-       JLabel profileLabel = new JLabel(fprofileOne);
-       add(profileLabel);
-       profileLabel.setBounds(350, 40, 250, 250);
-
-
+        Image singupImg_2 = signupImg.getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT);
+        ImageIcon fprofileOne = new ImageIcon(singupImg_2);
+        JLabel profileLabel = new JLabel(fprofileOne);
+        add(profileLabel);
+        profileLabel.setBounds(350, 40, 250, 250);
 
         setLayout(null); // to remove default layout i.e border laypout from our frame
         setSize(640, 400);
         setLocation(500, 200);
-        setVisible(true); 
+        setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
+
     @Override
-    public void actionPerformed(ActionEvent e) 
-    {
-        if(e.getSource() == Create) //getSoucre returns on which button event is performed
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == Create) // getSoucre returns on which button event is performed
         {
-            String  sAccountas =  Accountas.getSelectedItem();
+            String sAccountas = Accountas.getSelectedItem();
             String susername = textusername.getText();
             String Sname = textname.getText();
             String Spassword = textpassword.getText();
             String Smeter = textmeterNo.getText();
-            
+
             try {
                 Database c = new Database();
                 String query = null;
-                query = "insert into Signup value('" +Smeter+"' , '" +susername+"' , '" +Sname+"' , '" +Spassword+"' , '" +sAccountas+"')";
+                query = "insert into Signup value('" + Smeter + "' , '" + susername + "' , '" + Sname + "' , '"
+                        + Spassword + "' , '" + sAccountas + "')";
                 c.statement.executeUpdate(query);
 
                 // To show account is cerated
@@ -162,15 +187,13 @@ public class Signup extends JFrame implements ActionListener
             } catch (Exception databse_E) {
                 databse_E.printStackTrace();
             }
-        }
-        else if(e.getSource() == Back)
-        {
+        } else if (e.getSource() == Back) {
             setVisible(false);
             new Login();
         }
     }
-    public static void main(String[] args) 
-    {
+
+    public static void main(String[] args) {
         new Signup();
     }
 }
--- electricity/billing/system/main_class.java
+++ electricity/billing/system/main_class.java
@@ -3,6 +3,9 @@ package electricity.billing.system;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -10,12 +13,10 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-public class main_class extends JFrame
-{
-    main_class()
-    {
+public class main_class extends JFrame implements ActionListener {
+    main_class() {
         // Shortcut to set visiblity
-        setExtendedState(JFrame.MAXIMIZED_BOTH); //Get Frame of size of our screen
+        setExtendedState(JFrame.MAXIMIZED_BOTH); // Get Frame of size of our screen
 
         // Setting Image
         ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icons/ebs.png"));
@@ -26,88 +27,89 @@ public class main_class extends JFrame
 
         // Creating Menu bar
         JMenuBar menubar = new JMenuBar();
-        setJMenuBar(menubar); // To add menu bar to frame 
-        
-        //Adding Componets to menu bar named as Menu
+        setJMenuBar(menubar); // To add menu bar to frame
+
+        // Adding Componets to menu bar named as Menu
         JMenu menu = new JMenu("Menu");
-        menu.setFont(new Font("serif",Font.PLAIN,15));//To set font of text
-        menubar.add(menu); //Add tab to menu bar
-        //Add sub comopent to menu bar with image
+        menu.setFont(new Font("serif", Font.PLAIN, 15));// To set font of text
+        menubar.add(menu); // Add tab to menu bar
+        // Add sub comopent to menu bar with image
         // New Coustomer
         JMenuItem newCoustomer = new JMenuItem("New Coustomer");
-        newCoustomer.setFont(new Font("monospaced",Font.PLAIN,12));
+        newCoustomer.setFont(new Font("monospaced", Font.PLAIN, 12));
         ImageIcon coutomerImage = new ImageIcon(ClassLoader.getSystemResource("icons/newcustomer.png"));
         Image coustmerimg = coutomerImage.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         newCoustomer.setIcon(new ImageIcon(coustmerimg));
         menu.add(newCoustomer);
+
         // Coustomer Detail
         JMenuItem Coustomerdetail = new JMenuItem("Deposit Details");
-        Coustomerdetail.setFont(new Font("monospaced",Font.PLAIN,12));
+        Coustomerdetail.setFont(new Font("monospaced", Font.PLAIN, 12));
         ImageIcon detailsImage = new ImageIcon(ClassLoader.getSystemResource("icons/customerDetails.png"));
         Image detailimg = detailsImage.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         Coustomerdetail.setIcon(new ImageIcon(detailimg));
         menu.add(Coustomerdetail);
         // Deposit details
         JMenuItem depositdetails = new JMenuItem("Coustomer Details");
-        depositdetails.setFont(new Font("monospaced",Font.PLAIN,12));
+        depositdetails.setFont(new Font("monospaced", Font.PLAIN, 12));
         ImageIcon detailsdeposit = new ImageIcon(ClassLoader.getSystemResource("icons/depositdetails.png"));
         Image detaildepositimg = detailsdeposit.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         depositdetails.setIcon(new ImageIcon(detaildepositimg));
         menu.add(depositdetails);
         // Calculate Bills
         JMenuItem Calculatebill = new JMenuItem("Calculate Bill");
-        Calculatebill.setFont(new Font("monospaced",Font.PLAIN,12));
+        Calculatebill.setFont(new Font("monospaced", Font.PLAIN, 12));
         ImageIcon CalculatebillIamge = new ImageIcon(ClassLoader.getSystemResource("icons/calculatorbills.png"));
-        Image  Calculatebillimg =  CalculatebillIamge.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
+        Image Calculatebillimg = CalculatebillIamge.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         Calculatebill.setIcon(new ImageIcon(Calculatebillimg));
         menu.add(Calculatebill);
 
         // Addong New tab to menu bar named as Information
         JMenu info = new JMenu("Information");
-        info.setFont(new Font("serif",Font.PLAIN,15));
+        info.setFont(new Font("serif", Font.PLAIN, 15));
         menubar.add(info);
         // Adding componets to INformation tab
         // Update information
         JMenuItem upinfo = new JMenuItem("Update Information");
-        upinfo.setFont(new Font("monospaced",Font.PLAIN,12));
+        upinfo.setFont(new Font("monospaced", Font.PLAIN, 12));
         ImageIcon upinfoimage = new ImageIcon(ClassLoader.getSystemResource("icons/refresh.png"));
         Image upinfoimg = upinfoimage.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         upinfo.setIcon(new ImageIcon(upinfoimg));
         info.add(upinfo);
         // View Information
         JMenuItem viewinfo = new JMenuItem("View Information");
-        viewinfo.setFont(new Font("monospaced",Font.PLAIN,12));
+        viewinfo.setFont(new Font("monospaced", Font.PLAIN, 12));
         ImageIcon viewinfoImage = new ImageIcon(ClassLoader.getSystemResource("icons/information.png"));
         Image viewinfoimg = viewinfoImage.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         viewinfo.setIcon(new ImageIcon(viewinfoimg));
         info.add(viewinfo);
 
-        //Addong New tab to menu bar named as  User 
+        // Addong New tab to menu bar named as User
         JMenu user = new JMenu("User");
-        user.setFont(new Font("serif",Font.PLAIN,15));
+        user.setFont(new Font("serif", Font.PLAIN, 15));
         menubar.add(user);
         // Adding componets to USER tab
         JMenuItem paybill = new JMenuItem("Paybill");
-        paybill.setFont(new Font("monospaced",Font.PLAIN,12));
+        paybill.setFont(new Font("monospaced", Font.PLAIN, 12));
         ImageIcon paybillimage = new ImageIcon(ClassLoader.getSystemResource("icons/pay.png"));
         Image paybillimg = paybillimage.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         paybill.setIcon(new ImageIcon(paybillimg));
         user.add(paybill);
         // Bill details
         JMenuItem billdetails = new JMenuItem("Bill Detalis");
-        billdetails.setFont(new Font("monospaced",Font.PLAIN,12));
+        billdetails.setFont(new Font("monospaced", Font.PLAIN, 12));
         ImageIcon billdetailsimage = new ImageIcon(ClassLoader.getSystemResource("icons/details.png"));
         Image billdetailsimg = billdetailsimage.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         billdetails.setIcon(new ImageIcon(billdetailsimg));
         user.add(billdetails);
 
-        //Adding New tab to menu bar named as Bill
+        // Adding New tab to menu bar named as Bill
         JMenu bill = new JMenu("Bill");
-        bill.setFont(new Font("serif",Font.PLAIN,15));
+        bill.setFont(new Font("serif", Font.PLAIN, 15));
         menubar.add(bill);
         // Generate bill
         JMenuItem genBill = new JMenuItem("Generate Bill");
-        genBill.setFont(new Font("monospaced",Font.PLAIN,12));
+        genBill.setFont(new Font("monospaced", Font.PLAIN, 12));
         ImageIcon genbillimage = new ImageIcon(ClassLoader.getSystemResource("icons/bill.png"));
         Image genbillimg = genbillimage.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         genBill.setIcon(new ImageIcon(genbillimg));
@@ -115,45 +117,51 @@ public class main_class extends JFrame
 
         // Adding new tab to menu bar named as utility
         JMenu utility = new JMenu("Utility");
-        utility.setFont(new Font("serif",Font.PLAIN,15));
+        utility.setFont(new Font("serif", Font.PLAIN, 15));
         menubar.add(utility);
         // Adding Componets to utility tab
         // Note Pad
         JMenuItem notepad = new JMenuItem("Notepad");
-        notepad.setFont(new Font("monospaced",Font.PLAIN,12));
+        notepad.setFont(new Font("monospaced", Font.PLAIN, 12));
         ImageIcon notepadimage = new ImageIcon(ClassLoader.getSystemResource("icons/notepad.png"));
         Image notepadimg = notepadimage.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         notepad.setIcon(new ImageIcon(notepadimg));
         utility.add(notepad);
         // Bill Calculator
         JMenuItem Billcalculator = new JMenuItem("Calculator");
-        Billcalculator.setFont(new Font("monospaced",Font.PLAIN,12));
+        Billcalculator.setFont(new Font("monospaced", Font.PLAIN, 12));
         ImageIcon Billcalculatorimage = new ImageIcon(ClassLoader.getSystemResource("icons/calculatorbills.png"));
         Image Billcalculatorimg = Billcalculatorimage.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         Billcalculator.setIcon(new ImageIcon(Billcalculatorimg));
-        // Billcalculator.setBackground(new Color()); to change background color using RGB Value
+        // Billcalculator.setBackground(new Color()); to change background color using
+        // RGB Value
         utility.add(Billcalculator);
 
         // Adding New Tab to menu bar as Exit
         JMenu exit = new JMenu("Exit");
-        exit.setFont(new Font("serif",Font.PLAIN,15));
+        exit.setFont(new Font("serif", Font.PLAIN, 15));
         menubar.add(exit);
         // Adding Componets to utility tab
         // Note Pad
         JMenuItem eexit = new JMenuItem("Exit");
-        eexit.setFont(new Font("monospaced",Font.PLAIN,12));
+        eexit.setFont(new Font("monospaced", Font.PLAIN, 12));
         ImageIcon eexitimage = new ImageIcon(ClassLoader.getSystemResource("icons/exit.png"));
         Image eexitimg = eexitimage.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         eexit.setIcon(new ImageIcon(eexitimg));
         exit.add(eexit);
-
 
         setLayout(new FlowLayout());
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
     }
+
     public static void main(String[] args) {
         new main_class();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
